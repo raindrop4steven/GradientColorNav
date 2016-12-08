1ab5dfd1853a12dd81ba43ec8319beea0a5e04f8
steven <rockboyfor@gmail.com>
2016-12-08 22:53:55 +0800

    Initial Commit

---
+++ GradientColorNav/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradientColorNav
+//
+//  Created by steven on 2016/12/8.
+//  Copyright © 2016年 steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GradientColorNav/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientColorNav
+//
+//  Created by steven on 2016/12/8.
+//  Copyright © 2016年 steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
